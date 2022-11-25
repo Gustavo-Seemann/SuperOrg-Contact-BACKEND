26c753a4ae0e6da1ce52afc433757e305767c37a
Luiz Gustavo Seemann <101838119+Gustavo-Seemann@users.noreply.github.com>
2022-11-24 23:13:17 -0300

    refact: finalizacoes config api

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask 
-from flask import Blueprint, request, json
+from flask import Blueprint, request, json, jsonify
 from flask.wrappers import Response
 from flask.globals import session
 from utils import callback_google, flow
@@ -8,6 +8,7 @@ from flask import request, current_app
 from werkzeug.utils import redirect
 from waitress import serve
 from config import app_config
+from utils import exist_key, get_user_contacts
 
 
 app = Flask(__name__)
@@ -33,4 +34,30 @@ def auth_google():
 @app.route('/users/callback', methods = ["GET"])
 def callback():
     data = callback_google()
-    return redirect(f"http://localhost:8080/login?token={data['token']}&expiry={data['expiry']}&picture={data['user_image']}&user_name={data['user_name']}")
+    return redirect(f"http://localhost:3000/login?token={data['token']}&expiry={data['expiry']}&picture={data['user_image']}&user_name={data['user_name']}")
+
+@app.route('/contacts/get', methods = ["POST"])
+def get_contacts():
+
+    list_keys = ["token"]
+    token = exist_key(request.get_json(), list_keys)
+
+    if 'error' in token:
+        return jsonify(token), 400
+
+    response = get_user_contacts(token['token'])
+
+    response_json = response.json()
+
+    if 'error' in response_json:
+        return Response(
+            response=response,
+            status=response_json['error']['code'],
+            mimetype='application/json'
+        )
+
+    return Response(
+        response=response,
+        status=200,
+        mimetype='application/json'
+    )  
--- utils/__init__.py
+++ utils/__init__.py
@@ -10,32 +10,33 @@ from google_auth_oauthlib.flow import Flow
 
 def callback_google():
 
-    flow.fetch_token(authorization_response = request.url)
-    credentials = flow.credentials
-    request_session = requests.session()
-    token_google = auth.transport.requests.Request(session=request_session)
-    user_google_dict = id_token.verify_oauth2_token(
-        id_token = credentials.id_token,
-        request=token_google,
-        audience=os.getenv("GOOGLE_CLIENT_ID"),
-        clock_skew_in_seconds=0
-    )
+    try: 
+        flow.fetch_token(authorization_response = request.url)
+        credentials = flow.credentials
+        request_session = requests.session()
+        token_google = auth.transport.requests.Request(session=request_session)
+        user_google_dict = id_token.verify_oauth2_token(
+            id_token = credentials.id_token,
+            request=token_google,
+            audience=os.getenv("GOOGLE_CLIENT_ID"),
+            clock_skew_in_seconds=0
+        )
 
-    data_auth = {
-        "user_name": "",
-        "user_image": "",
-        "token": "",
-        "expiry": ""
-    }
+        data_auth = {
+            "user_name": "",
+            "user_image": "",
+            "token": "",
+            "expiry": ""
+        }
 
-    data_auth['user_name'] = user_google_dict['name']
-    data_auth['user_image'] = user_google_dict['picture']
-    data_auth['token'] = credentials.token
-    data_auth['expiry'] = credentials.expiry
+        data_auth['user_name'] = user_google_dict['name']
+        data_auth['user_image'] = user_google_dict['picture']
+        data_auth['token'] = credentials.token
+        data_auth['expiry'] = credentials.expiry
 
-    return data_auth
-
-
+        return data_auth
+    except Exception as e:
+        print(e)
 
 
 flow = Flow.from_client_secrets_file(
@@ -45,7 +46,34 @@ flow = Flow.from_client_secrets_file(
         "https://www.googleapis.com/auth/userinfo.profile",
         "openid"
     ],
-    redirect_uri = "http://localhost:5000/users/callback"
+    redirect_uri = "http://localhost:8080/users/callback"
 )
 
 
+mimetype = 'application/json'
+
+def get_user_contacts(token):
+    headers = {
+        'Content-type': mimetype,
+        'Accept': mimetype
+    }
+    headers['Authorization'] = f"Bearer {token}"
+    url = 'https://people.googleapis.com/v1/people/me/connections?personFields=names,emailAddresses'
+
+    response = requests.get(url, headers=headers)
+    return response
+
+
+def exist_key(request_json, list_keys):
+    keys_not_have_in_request = []
+
+    for key in list_keys:
+        if key in request_json:
+            continue
+        else:
+            keys_not_have_in_request.append(key)
+
+    if len(keys_not_have_in_request) == 0:
+        return request_json
+
+    return {"error": f"Está faltando o item {keys_not_have_in_request}"}
